4dfe5560d781042609b3a183865324333abe5b0e
Sergey Starostin <adelaide.star@gmail.com>
2022-08-08 10:31:57 +0300

    Added workplace

---
+++ src/test/java/Experiments/Faker1.java
@@ -0,0 +1,18 @@
+package Experiments;
+
+import org.junit.jupiter.api.Test;
+
+public class Faker1 {
+
+    public static class Faker {
+
+        @Test
+        public static void main(String[] args) {
+            com.github.javafaker.Faker faker = new com.github.javafaker.Faker();
+
+            for (int i = 0; i < 100; i++) {
+                System.out.println(faker.internet().macAddress());
+            }
+        }
+    }
+}
