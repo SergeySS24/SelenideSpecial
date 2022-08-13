eeefb7290916697995f6d60630b42221eac528bf
Sergey Starostin <adelaide.star@gmail.com>
2022-08-13 22:42:58 +0300

    Added selenide test

--- src/test/java/com/MyCode/FirstAttempt.java
+++ src/test/java/com/MyCode/FirstAttempt.java
@@ -1,17 +1,31 @@
 package com.MyCode;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.*;
 
 public class FirstAttempt {
+
+    @BeforeAll
+    public static void preparation() {
+        //Configuration.holdBrowserOpen = true;
+    }
     @Test
     public void findSelenide() {
         Selenide.open("https://github.com/");
         $(".header-search-input").setValue("Selenide").pressEnter();
-        $("f4").$("v-align-middle").find(byText("selenide/selenide")).click();
+        $$("ul.repo-list li").first().$("a").click();
+        $("#repository-container-header").shouldHave(text("selenide / selenide"));
+
+
+
+
 
 
     }
