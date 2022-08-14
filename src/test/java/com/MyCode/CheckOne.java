275c2bc08a98300b98761e520204c10322e33e6e
Sergey Starostin <adelaide.star@gmail.com>
2022-08-14 22:58:29 +0300

    Added selenide test 2

--- src/test/java/com/MyCode/CheckOne.java
+++ src/test/java/com/MyCode/CheckOne.java
@@ -1,16 +1,13 @@
 package com.MyCode;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class FirstAttempt {
+public class CheckOne {
 
     @BeforeAll
     public static void preparation() {
---
+++ src/test/java/com/MyCode/CheckTwo.java
@@ -0,0 +1,34 @@
+package com.MyCode;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CheckTwo {
+
+    @BeforeAll
+    public static void preparation() {
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    public void findContributor() {
+        Selenide.open("https://github.com/selenide/selenide");
+
+        //$("Layout-sidebar").$(byText("Contributors")).ancestor("div").$$("ul li").first().hover();
+
+        $(".Layout-sidebar").$(byText("Contributors")).ancestor("div").$$("ul li").first().hover();
+        //$(".Popover-message").shouldHave(text("asolntsev"));
+        $$(".Popover-message").first().shouldHave(text("asolntsev"));
+                //.findBy(text("Contributors"))
+                //.ancestor("div").$$("ul li").first().hover();
+
+                //find(text("Contributors"));
+        sleep(500);
+    }
+}
