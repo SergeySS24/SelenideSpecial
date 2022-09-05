baf5642d3d332317708b3cf1c3ec50225047f887
Sergey Starostin <adelaide.star@gmail.com>
2022-09-06 01:19:08 +0300

    Added useless branch

--- src/test/java/com/MyCode/FirstGo.java
+++ src/test/java/com/MyCode/FirstGo.java
@@ -33,7 +33,21 @@ public class FirstGo {
     @Test
     void checkMouseClick() {
         Selenide.open("https://ru.shein.com/Home");
-        actions().moveByOffset(323, 111).click().perform();
+        //sleep(200);
+       // actions().moveByOffset(475, 111).click().perform();
+        actions().moveByOffset(82, 22).click().perform();
+    }
+
+    @Test
+    void checkMouseClick2() {
+        Selenide.open("https://piter.allithave.ru/");
+        sleep(100);
+        // actions().moveByOffset(475, 111).click().perform();
+       // actions().moveByOffset(400, 200).click().perform(); //каталог
+        //actions().moveByOffset(320, 200).click().perform(); //каталог левый край
+       // actions().moveByOffset(700, 190/205).click().perform(); //хиты продаж
+       // actions().moveByOffset(750, 110/115).click().perform(); //строка каталога
+        //actions().moveByOffset(730, 15).click().perform(); //мой заказ
 
     }
 
